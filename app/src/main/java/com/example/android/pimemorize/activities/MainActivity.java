1cf36a2f7f257022aae6aca5dbdda0d820bfcf43
Michael Min <minmichaelmm@gmail.com>
2016-03-16 22:27:53 -0400

    Switched to toolbar from action bar

--- app/src/main/java/com/example/android/pimemorize/activities/MainActivity.java
+++ app/src/main/java/com/example/android/pimemorize/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
@@ -35,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements NumPadFragment.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         View mHeaderLayout = findViewById(R.id.top_list_header);
         mCurrentRowTextView = (TextView) mHeaderLayout.findViewById(R.id.current_row_text_view);
